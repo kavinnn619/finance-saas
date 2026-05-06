9e1c6c141ce5206bfdad21f48c8dbe944d1c9c55
kavinnn619 <115263606+kavinnn619@users.noreply.github.com>
2026-05-07 04:16:03 +0530

    Delete frontend directory

--- frontend/src/hooks/useTransactions.ts
+++
@@ -1,9 +0,0 @@
-import { useInfiniteQuery } from '@tanstack/react-query';
-  import api from '../../utils/api';
-
-  export const useTransactions = () => useInfiniteQuery({
-    queryKey: ['transactions'],
-    queryFn: ({ pageParam }) => api.get('/api/transactions', { params: { cursor: pageParam } }).then(res => res.data),
-    getNextPageParam: (lastPage) => lastPage.nextCursor,
-    initialPageParam: undefined,
-  });
--- frontend/src/utils/constants.ts
+++
@@ -1,2 +0,0 @@
- export const ACCESS_TOKEN_KEY = 'accessToken';
-  export const USER_KEY = 'user';
