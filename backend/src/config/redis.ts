5c5da1a695b618a1bb5c64329baa47f6ffdd81ab
kavinnn619 <115263606+kavinnn619@users.noreply.github.com>
2026-05-07 03:43:04 +0530

    Delete backend/src directory

--- backend/src/config/redis.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'redis';
-  import { env } from './env';
-
-  export const redisClient = createClient({ url: env.REDIS_URL });
-
-  redisClient.on('error', (err) => {
-    console.error('Redis error:', err);
-  });
