d792c02501f0d66f1ce2e444f24524bdbbcd5b7a
kavinnn619 <115263606+kavinnn619@users.noreply.github.com>
2026-05-07 03:42:06 +0530

    Delete backend/src/config/cors.ts

--- backend/src/config/cors.ts
+++
@@ -1,8 +0,0 @@
-import cors from 'cors';
-  import { env } from './env';
-
-  export const corsConfig = cors({
-    origin: env.ALLOWED_ORIGINS,
-    credentials: true,
-    optionsSuccessStatus: 200,
-  });
